17a253a49eb08fe3841a2ec6859e32106f33b669
Patryk <patrykradzim24@gmail.com>
2023-09-08 09:29:59 +0200

    silnia

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Liczba: ");
+        int l1 = scanner.nextInt();
+        int l2 = 1;
+        for(int i=1; i<=l1; i++){
+            l2 = l2*i;
         }
+        System.out.println(l2);
     }
 }
